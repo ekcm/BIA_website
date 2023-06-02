2883759517ba9a4e4ccdfe5e78ea813483977d2e
ekcm <86366443+ekcm@users.noreply.github.com>
2023-06-03 02:21:21 +0800

    Creating website

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './assets/BIA_Logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className='logo' />
+        <h1>Hello Xuan Kai</h1>
+        <p>actually idk how to use react lol</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
