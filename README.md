be6628f19318b0d49ffef5a4d2e73f6e8435247e
ekcm <86366443+ekcm@users.noreply.github.com>
2023-06-03 02:54:50 +0800

    creating website again

--- README.md
+++
@@ -1,2 +0,0 @@
-# BIA_website
- 
--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,15 @@
+import logo from './logo.svg';
 import './App.css';
-import logo from './assets/BIA_Logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className='logo' />
         <h1>Hello Xuan Kai</h1>
-        <p>actually idk how to use react lol</p>
+
+        <p>bro idk react too lol</p>
+
+        <button type="submit" class="btn btn-primary">Testing bootstrap</button>
       </header>
     </div>
   );
