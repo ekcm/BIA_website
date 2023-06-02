d8af08a0f7e6e08fea317ece5a291a1edbb7d341
ekcm <86366443+ekcm@users.noreply.github.com>
2023-06-03 02:58:55 +0800

    adding assets

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/BIA_Logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className='logo'/>
+        
         <h1>Hello Xuan Kai</h1>
 
         <p>bro idk react too lol</p>
