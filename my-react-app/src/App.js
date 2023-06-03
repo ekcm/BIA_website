b9b7e86e2bc62815f8f9b4b2c649947c00d8123c
ekcm <86366443+ekcm@users.noreply.github.com>
2023-06-03 21:19:06 +0800

    adding nav bar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,13 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className='logo'/>
         
         <h1>Hello Xuan Kai</h1>
 
         <p>bro idk react too lol</p>
 
-        <button type="submit" class="btn btn-primary">Testing bootstrap</button>
+        
       </header>
     </div>
   );
---
+++ my-react-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import logo from "../../assets/BIA_Logo.png";
+import React from "react";
+import "../Navbar/Navbar.css";
+
+function NavBar() {
+  return (
+    <div class="navbar">
+      <img src={logo} className="logo" />
+
+      <div>
+        <div class="btn-group">
+          <button class="btn">Home</button>
+          <button class="btn dropdown">What we do</button>
+          <button class="btn dropdown">Get involved</button>
+        </div>
+        <button class="btn" id="prospectus">
+          Download Prospectus
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
