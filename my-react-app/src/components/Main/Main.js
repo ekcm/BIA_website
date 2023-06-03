cc06a6a73a26e92f24cd76c2fa36bbcaea7c1eca
ekcm <86366443+ekcm@users.noreply.github.com>
2023-06-04 01:02:42 +0800

    added vision

--- my-react-app/src/components/Main/Main.js
+++ my-react-app/src/components/Main/Main.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "../Main/Main.css";
-import image from "../../assets/Home_main.jpg"
+import image from "../../assets/Home_main.jpg";
 
-function Main_image(){
-    return(
-        <div>
-            <img src={image} className="image"/>
-            <h2>We are</h2>
-            <h1>SMUBIA</h1>
+function Main_image() {
+  return (
+    <div>
+      <div className="container-fluid">
+        <img src={image} className="image" />
+        <div class="bottom-left">
+          <h2>We are</h2>
+          <h1>SMUBIA</h1>
         </div>
-        
-    )
+      </div>
+      <div className="container-fluid" id="box">
+        <h4 id="vision" >Our vision:</h4>
+        <h4 id="SMU">To build a sustainable ecosystem for analytics in SMU</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Main_image;
