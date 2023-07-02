0b797f5bfe1ed8ba7bfa5ede202ca027b7c28466
sliponsandpaper12 <135314240+sliponsandpaper12@users.noreply.github.com>
2023-07-02 21:04:02 +0800

    Update Partners.js

--- my-react-app/src/components/Partners/Partners.js
+++ my-react-app/src/components/Partners/Partners.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./Partners.css";
+import sginno from "../../assets/sginno.png";
+import ihis from "../../assets/ihis.ico";
+import govtech from "../../assets/govtech.png";
+import ey from "../../assets/eylogo.png";
+import alibaba from "../../assets/alibaba-cloud-logo.webp";
+import tableau from "../../assets/Tableau-Emblem.png";
+
 
 function Partners() {
     return(
-        <div>
-            
+        <div className="partners">
+            <div className="partner-row">
+            <img src={sginno} alt=""/>
+            <img src={ihis} alt=""/>
+            <img src={govtech} alt=""/>
+            </div>
+            <div className="partner-row">
+            <img src={ey} alt="" />
+            <img src={alibaba} alt=""/>
+            <img src={tableau} alt=""/>
+            </div>
         </div>
     )
 }
